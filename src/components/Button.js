9d7666b8271c2539ec03d8d79bd7ec29ca211c9f
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-11-18 13:25:52 +0000

    Update

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledButton = styled.button`
     padding: 1rem;
@@ -8,12 +8,29 @@ const StyledButton = styled.button`
     color: ${({ primary }) => (primary ? "yellow" : "#fff")};
     border: none;
     background-color: gray;
-    margin: 10px;
+    margin: auto;
+    margin-bottom: 10px;
     width: 250px;
+    ${({ primary }) =>
+        primary &&
+        css`
+            color: white;
+            background-color: palevioletred;
+        `}
 `;
 
-export const Button = ({ primary, children }) => {
-    return <StyledButton primary={primary}>{children}</StyledButton>;
+const SuperButton = styled(StyledButton)`
+    font-size: 2.5rem;
+    color: yellow;
+`;
+
+const Button = ({ primary, children }) => {
+    return (
+        <>
+            <StyledButton primary={primary}>{children}</StyledButton>
+            <SuperButton>{children}</SuperButton>
+        </>
+    );
 };
 
 export default Button;
